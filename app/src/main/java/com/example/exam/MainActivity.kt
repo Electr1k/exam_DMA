196fa81abf71c451e8ef40bef5a02e5f11ff93ac
Electr1k <gynter37@gmail.com>
2024-01-23 11:36:30 +0300

    Added add book

--- app/src/main/java/com/example/exam/MainActivity.kt
+++ app/src/main/java/com/example/exam/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.exam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rv: RecyclerView
+    private lateinit var up_btn: ImageView
+    private lateinit var bottomSheet: LinearLayout
+    private lateinit var behavior: BottomSheetBehavior<LinearLayout>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         rv = findViewById(R.id.RV)
+        up_btn = findViewById(R.id.up_menu)
+        bottomSheet = findViewById(R.id.bottom_sheet)
+        behavior = BottomSheetBehavior.from(bottomSheet)
 
+        up_btn.setOnClickListener{
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
         val booksList = listOf(
             Book("Книга1", "Марк Твен", 1),
             Book("Книга2", "Стивен Хоккинг", 1),
